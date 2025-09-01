23a01a218aa339005bac6511d969de70849a49eb
Sherlie <sor2255146@slu.edu.ph>
2025-09-02 05:48:02 +0800

    Add files via upload

---
+++ Data Structures/src/prelim/ListOverflowException.java
@@ -0,0 +1,9 @@
+package prelim;
+public class ListOverflowException extends Exception {
+    public ListOverflowException() {
+        super("List is full! Cannot insert more elements.");
+    }
+    public ListOverflowException(String message) {
+        super(message);
+    }
+}
